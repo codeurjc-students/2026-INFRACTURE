d827b93fa0cc5750b5d985542b4f4073c3703b2a
pruizz <lttpablo@gmail.com>
2026-09-01 18:33:25 +0200

    feat: add backend with PostgreSQL and Flyway

---
+++ backend/src/main/java/es/codeurjc/infracture/InfractureBackendApplication.java
@@ -0,0 +1,13 @@
+package es.codeurjc.infracture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfractureBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfractureBackendApplication.class, args);
+	}
+
+}
